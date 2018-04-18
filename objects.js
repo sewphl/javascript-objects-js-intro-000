9c4016d6f264a7086d7d6126a2a6054810b3ee38
sharon <sharonoptsout@gmail.com>
2018-04-18 21:21:43 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -3,9 +3,8 @@ var playlist = {
   'bon jovi': "you give love a bad name"
 };
 
-/*
+
 function updatePlaylist(playlist, artistName, songTitle) {
   playlist.artistName = [songTitle];
   return(playlist);
 }
-*/
